4f35ceca1457a10973b1118526412dcb14f3a8a9
lahatra3 <lahatrap20.aps2a@gmail.com>
2022-04-12 16:28:30 +0300

    [ADD] fonction qui prend les valeurs entrées...

--- calculateur/src/app/home-section/home-section.component.ts
+++ calculateur/src/app/home-section/home-section.component.ts
@@ -7,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeSectionComponent implements OnInit {
 
+  input!: string | number;
+  output!: number;
+  firstLine: Array<number> = [1, 2, 3];
+  secondLine: Array<number> = [4, 5, 6];
+  thirdLine: Array<number> = [7, 8, 9]
+
   constructor() { }
 
   ngOnInit(): void {
-    
+    this.input = "0";
+    this.output = 0;
+  }
+
+  onGetValue(valeur: number|string): void {
+    this.input = valeur;
   }
 
 }
